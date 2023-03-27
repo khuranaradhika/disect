852dd0affd28b79dacfaa1c265ada3db30e93095
Radhika Khurana <mail.radhikakhurana@gmail.com>
2023-03-26 21:25:17 -0400

    Update playground.js
    created the first charity

--- database/playground.js
+++ database/playground.js
@@ -5,14 +5,16 @@
 
 // Select the database to use.
 use('mongodbVSCodePlaygroundDB');
+use disect
+db.createCollection("charities")
 
 // The drop() command destroys all data from a collection.
 // Make sure you run it against the correct database and collection.
 db.sales.drop();
 
 // Insert a few documents into the sales collection.
-db.sales.insertMany([
-  { '_id': 1, 'item': 'abc', 'price': 10, 'quantity': 2, 'date': new Date('2014-03-01T08:00:00Z') },
+db.charities.insertMany([
+  { '_id': 1, 'category': 'Labor Rights', 'name': 'Clean Clothes Campaign', 'main_website_url': 'https://cleanclothes.org/', 'donation_url': 'https://cleanclothes.org/donate', 'short_description': 'The Clean Clothes Campaign is a global network of over 235 organizations in more than 45 countries. It aims to improve the working conditions of the garment and sportswear industries by connecting various organizations, including grassroots unions, trade unions, and feminist organizations. The campaign is worker-centered and utilizes a horizontal operational structure to address local and global problems. They offer direct solidarity support to workers fighting against specific worker rights violations and co-develop global campaigns to achieve systemic goals. The campaign educates and mobilizes consumers, lobbies companies and governments, and cooperates with other labor rights campaigns. All members, partners, and associates are dedicated to empowering workers to ensure their fundamental rights are respected.' },
   { '_id': 2, 'item': 'jkl', 'price': 20, 'quantity': 1, 'date': new Date('2014-03-01T09:00:00Z') },
   { '_id': 3, 'item': 'xyz', 'price': 5, 'quantity': 10, 'date': new Date('2014-03-15T09:00:00Z') },
   { '_id': 4, 'item': 'xyz', 'price': 5, 'quantity':  20, 'date': new Date('2014-04-04T11:21:39.736Z') },
