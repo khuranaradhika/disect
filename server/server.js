fac7a73dbb0e0c970f5e9b27202c2e454614482d
Tyler Baughcome <tbaughcome@gmail.com>
2023-03-27 16:06:45 -0400

    Express server + MongoDB connection established

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+//connect Database
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extend: false }));
+
+app.get('/', (req, res) => res.send('API Running'));
+//Define Route
+//...
+
+// Check if the slack channel is workingfsdsdfsdkhfajksdfhjklashfajklsd
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
