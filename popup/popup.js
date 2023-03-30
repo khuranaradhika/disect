ff705ccde10fd37780b364e7cd9adfc6ce57103b
Radhika Khurana <mail.radhikakhurana@gmail.com>
2023-03-30 17:10:12 -0400

    Color functionality to popup.js

---
+++ popup/popup.js
@@ -0,0 +1,22 @@
+// Example backend value
+const  = 42;
+
+// Example function to determine color based on backend value
+function getColor(value) {
+  if (value < 50) {
+    return 'red';
+  } else if (value < 100) {
+    return 'yellow';
+  } else {
+    return 'green';
+  }
+}
+
+// Get a reference to the HTML element
+const element = document.getElementById('myElement');
+
+// Get the color based on the backend value
+const color = getColor(valueFromBackend);
+
+// Set the background color of the element
+element.style.backgroundColor = color;
