311f1adcfaedbd7582b167a7e580988efb5093b4
Tyler Baughcome <tbaughcome@gmail.com>
2023-04-02 15:15:54 -0400

    background change on opening + dynamic emissions value change

---
+++ client/popup/popup.js
@@ -0,0 +1,28 @@
+// Example function to determine color based on backend value
+function getColor(value) {
+  if (value < 4) {
+    return 'red';
+  } else if (4 <value < 7) {
+    return 'yellow';
+  } else if (value > 7) {
+    return 'green';
+  }
+}
+window.onload = function() {
+  // Get the content of the element and convert it to a number
+  const myElement = document.getElementById('ethic-rater-rating-id') ;
+  const myNumber = parseFloat(myElement.textContent);
+
+  // Get a reference to the HTML element
+  const element = document.getElementById('ethic-rater-circle-id');
+
+  // Get the color based on the backend value
+  const color = getColor(myNumber);
+  console.log(color);
+
+  // Set the background color of the element
+  element.style.backgroundColor = color;
+
+  document.getElementById("co2-units-display-id").innerHTML = 4;
+
+}
